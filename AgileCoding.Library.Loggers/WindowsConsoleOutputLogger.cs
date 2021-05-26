d9405173747a50b4cca443b757df1e0fa488e196
Ernie <gunning.ernie@gmail.com>
2021-05-26 22:51:29 +0200

    Moved Logger implementations out of solution folder to prevent a messup on the namespace Added the WindowsConsoleOutputLogger that allows logging to the console. It uses the standardError stream for critical and error logging. The standard stream is used for the rest.

---
+++ AgileCoding.Library.Loggers/WindowsConsoleOutputLogger.cs
@@ -0,0 +1,78 @@
+﻿namespace AgileCoding.Library.Loggers.Console
+{
+    using AgileCoding.Extentions.Exceptions;
+    using AgileCoding.Library.Enums.Logging;
+    using AgileCoding.Library.Interfaces.Logging;
+    using System;
+    using System.IO;
+
+    internal class WindowsConsoleOutputLogger : ILogger
+    {
+        private StreamWriter standardOutput = null;
+
+        private StreamWriter standardErrorOutput = null;
+
+        public WindowsConsoleOutputLogger(StreamWriter standardOutput, StreamWriter standardErrorOutput)
+        {
+            this.standardErrorOutput = standardErrorOutput;
+            this.standardOutput = standardOutput;
+        }
+
+        public void Dispose()
+        {
+            this.standardOutput.Close();
+            this.standardErrorOutput.Close();
+            this.standardOutput.Dispose();
+            this.standardErrorOutput.Dispose();
+        }
+
+        public bool WriteCore(LogTypeEnum eventType, int eventId, object state, Exception exception, Func<object, Exception, string> formatter = null)
+        {
+            string exceptionMessage = exception != null ? exception.ToStringAll() : string.Empty;
+            string message = string.Empty;
+
+            switch (eventType)
+            {
+                case LogTypeEnum.Critical:
+                    message = string.Format("Critical Error: {0}{1}{1}Exception Details:{1}{2}", state.ToString(), Environment.NewLine, exceptionMessage);
+                    break;
+                case LogTypeEnum.Error:
+                    message = string.Format("{0}{1}{1}Exception Details:{1}{2}", state.ToString(), Environment.NewLine, exceptionMessage);
+                    break;
+                case LogTypeEnum.Warning:
+                    message = exception == null
+                        ? string.Format("Warning! : {0}", state.ToString())
+                        : string.Format("Warning! : {0}{1}{1}Exception Details:{1}{2}", state.ToString(), Environment.NewLine, exceptionMessage);
+                    break;
+                case LogTypeEnum.Information:
+                    message = exception == null
+                        ? string.Format("Informational : {0}", state.ToString())
+                        : string.Format("Informational : {0}{1}{1}Exception Details:{1}{2}", state.ToString(), Environment.NewLine, exceptionMessage);
+                    break;
+                case LogTypeEnum.Verbose:
+                    message = exception == null
+                        ? string.Format("Verbose Information : {0}", state.ToString())
+                        : string.Format("Verbose Information : {0}{1}{1}Exception Details:{1}{2}", state.ToString(), Environment.NewLine, exceptionMessage);
+                    break;
+                default:
+                    break;
+            }
+
+            if (UseStandardErrorOutput(eventType))
+            {
+                this.standardErrorOutput.WriteLine(message);
+            }
+            else
+            {
+                this.standardOutput.WriteLine(message);
+            }
+
+            return true;
+        }
+
+        private bool UseStandardErrorOutput(LogTypeEnum eventType)
+        {
+            return (eventType == LogTypeEnum.Error || eventType == LogTypeEnum.Critical || eventType == LogTypeEnum.Warning);
+        }
+    }
+}
---
+++ AgileCoding.Library.Loggers/WindowsEventLoggerExtentions.cs
@@ -0,0 +1,23 @@
+﻿namespace AgileCoding.Library.Loggers
+{
+    using AgileCoding.Library.Interfaces.Logging;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public static class WindowsEventLoggerExtentions
+    {
+        public static ILogger CreateWindowsEventLoggerInstance(this ILogger logger, string LogName, string sourceName)
+        {
+            var applicatonLog = EventLog.GetEventLogs()
+                .Where(x => x.Log.Equals(LogName))
+                .Single();
+
+            return new WindowsEventLogger(applicatonLog, sourceName);
+        }
+
+        public static ILogger CreateWindowsEventLoggerInstance(this ILogger logger, EventLog eventLog, string sourceName)
+        {
+            return new WindowsEventLogger(eventLog, sourceName);
+        }
+    }
+}
